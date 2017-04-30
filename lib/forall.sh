3c07c3f0e3f10d9c28dda20f40c5480aad88e7fd
August Valera <4U6U57@gmail.com>
2017-04-30 12:09:11 -0700

    Improvements for forall

--- lib/forall.sh
+++ lib/forall.sh
@@ -2,7 +2,7 @@
 
 # forall.sh
 # Runs input $CMD forall submissions in $ASG directory
-# Usage: forall.sh $ASG $CMD
+# Usage: forall.sh [-d] [$ASG] $CMD
 # Or, if running from within directory named $ASG (such as $CLASS/$ASG or $CLASS/bin/$ASG)
 #        forall.sh $CMD
 # August Valera <avalera>
@@ -13,6 +13,12 @@ ClassDir=$(echo $PwdDir | cut -d "/" -f -5)
 Class=$(basename $ClassDir)
 Asg=""
 
+ShowName=false
+if [[ $1 == "-d" ]]; then
+  echo "Displaying usernames"
+  ShowName=true
+  shift
+fi
 echo $ClassDir/$1
 if [[ -d "$ClassDir/$Pwd" && "$Pwd" != "bin" ]]; then
   Asg=$Pwd
@@ -35,5 +41,8 @@ cd $AsgDir
 for Student in $(ls -d */); do
   StudentDir=$AsgDir/$Student
   cd $StudentDir
+  if $ShowName; then
+    echo $Student
+  fi
   $@
 done
